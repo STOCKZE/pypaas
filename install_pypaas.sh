ed1f1a53ecf6751f61386b043574e2483362cbb0
STOCKZE <90406660+STOCKZE@users.noreply.github.com>
2023-09-06 01:37:50 +0400

    Update install_pypaas.sh

--- install_pypaas.sh
+++ install_pypaas.sh
@@ -19,22 +19,22 @@ sudo apt install -y docker-compose
 # Create a custom Docker network
 docker network create pass_network
 
-# Install Streamlit
-pip3 install streamlit
+# Install Streamlit & other paas.py dependencies
+pip3 install streamlit asyncio json subprocess logging aiofiles psutil matplotlib numpy time
 
 # Run common Docker containers
 
 # Keycloak
-docker run -d --name keycloak --network=pass_network -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -p 8080:8080 jboss/keycloak
+docker run -d  --restart=always --name keycloak --network=pass_network -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin -p 8080:8080 jboss/keycloak
 
 # Redis
-docker run -d --name redis --network=pass_network -p 6379:6379 redis
+docker run -d  --restart=always --name redis --network=pass_network -p 6379:6379 redis
 
 # NATS
-docker run -d --name nats --network=pass_network -p 4222:4222 -p 6222:6222 -p 8222:8222 nats
+docker run -d  --restart=always --name nats --network=pass_network -p 4222:4222 -p 6222:6222 -p 8222:8222 nats
 
 # PostgreSQL
-docker run -d --name postgres --network=pass_network -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin -e POSTGRES_DB=mydatabase -p 5432:5432 postgres
+docker run -d  --restart=always --name postgres --network=pass_network -e POSTGRES_USER=admin -e POSTGRES_PASSWORD=admin -e POSTGRES_DB=mydatabase -p 5432:5432 postgres
 
 # Create Caddyfile
 cat > Caddyfile <<EOL
@@ -57,6 +57,23 @@ docker run -d --name caddy --network=pass_network -p 80:80 -p 443:443 -v $(pwd)/
 
 # Download paas.py using wget
 wget "https://raw.githubusercontent.com/STOCKZE/pypaas/main/paas.py"
+
+# Create and enable a systemd service for the Streamlit app
+echo "[Unit]
+Description=Streamlit App Service
+After=network.target
+
+[Service]
+ExecStart=/usr/local/bin/streamlit run ~/app.py
+Restart=always
+User=yourusername
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/streamlit-app.service
+
+# Enable and start the Streamlit systemd service
+systemctl enable streamlit-app.service
+systemctl start streamlit-app.service
 
 # Run Streamlit app 
 streamlit run paas.py
